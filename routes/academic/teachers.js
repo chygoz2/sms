03df67abdc899718f774cccced778fcc060732d0
Chigozie Ekwonu <chygoz2@gmail.com>
2017-05-13 12:09:14 +0100

    Added searching for teachers by classes and subjects to the teachers API

--- routes/academic/teachers.js
+++ routes/academic/teachers.js
@@ -1,6 +1,8 @@
 var User = require('../../models/users_model');
 var express = require('express');
 var router = express.Router();
+var SessionClass = require('../../models/session_class_model');
+var SessionSubject = require('../../models/session_subject_model');
 
 router.get('/', function(req,res,next){
     User.find({role: 'teacher'}).exec(function(err,data){
@@ -83,6 +85,38 @@ router.delete('/delete/:_id', function(req,res,next){
         if(err) res.json({error: true, message: 'Error deleting teacher'});
         else res.json({error:false, message: 'Teacher deleted successfully'});
     })
-})
+});
+
+//get class teacher
+router.get('/class/:class', function (req,res,next) {
+    //check if class exists
+    SessionClass.findById(req.params.class).populate('teacher students classId').exec(function(err,data){
+        if(err){
+            res.json({error: true, message: 'Error occured while fetching class'});
+            return;
+        }
+        if(data == null){
+            res.json({error: true, message: 'Class not found'});
+            return;
+        }
+        res.json({error: false, message: data});
+    });
+});
+
+//get subject teacher
+router.get('/subject/:subject', function (req,res,next) {
+    //check if subject exists
+    SessionSubject.findById(req.params.subject).populate('teacher students subject').exec(function(err,data){
+        if(err){
+            res.json({error: true, message: 'Error occured while fetching subject'});
+            return;
+        }
+        if(data == null){
+            res.json({error: true, message: 'Subject not found'});
+            return;
+        }
+        res.json({error: false, message: data});
+    });
+});
 
 module.exports = router;
